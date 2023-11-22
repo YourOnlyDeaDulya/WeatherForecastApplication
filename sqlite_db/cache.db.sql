3538b3f7343d27db7ca726c806073b6c0087c75a
Ivan <122279435+YourOnlyDeaDulya@users.noreply.github.com>
2023-11-22 21:27:27 +0300

    Fixed db script

--- sqlite_db/cache.db.sql
+++ sqlite_db/cache.db.sql
@@ -32,11 +32,5 @@ CREATE TABLE IF NOT EXISTS "ForecastDays" (
 	"city"	TEXT NOT NULL,
 	FOREIGN KEY("city") REFERENCES "ForecastWeather"("city") ON DELETE CASCADE
 );
-INSERT INTO "CurrentWeather" ("city","country","day","month","year","hour","minute","weather_condition","celcium_t","feels_like","is_day","request_date") VALUES ('Москва','Россия',22,11,2023,20,1,'Небольшой снег',-8.0,-14.3,0,'2023-11-22');
-INSERT INTO "ForecastWeather" ("city","country","request_date") VALUES ('Москва','Россия','2023-11-22');
-INSERT INTO "ForecastDays" ("avg_t","min_t","max_t","weather_condition","day","month","year","request_date","city") VALUES (-7.5,-8.6,-6.5,'Переменная облачность',22,11,2023,'2023-11-22','Москва'),
- (-7.3,-8.6,-6.2,'Умеренный или сильный снег',23,11,2023,'2023-11-22','Москва'),
- (-4.2,-7.1,0.6,'Умеренный или сильный снег',24,11,2023,'2023-11-22','Москва'),
- (-1.9,-4.7,-0.7,'Местами умеренный снег',25,11,2023,'2023-11-22','Москва'),
- (-4.0,-5.3,-2.3,'Дымка',26,11,2023,'2023-11-22','Москва');
+
 COMMIT;
